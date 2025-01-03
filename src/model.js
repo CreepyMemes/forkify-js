ebf8a8ab134d8f89b6270a4e633d72858d6e7368
CreepyMemes <mjouins@gmail.com>
2025-01-03 06:58:46 +0100

    added saving query to state

--- src/model.js
+++ src/model.js
@@ -16,6 +16,8 @@ export const loadRecipe = async function (recipeId) {
 
 // Fetch recipe results by search keyword
 export const loadSearchResults = async function (query) {
+  state.search.query = query;
+
   const data = await getJSON(`${import.meta.env.VITE_API_URL}?search=${query}&key=${import.meta.env.VITE_API_KEY}`);
   state.search.results = await camelizeKeys(data.data.recipes);
 };
