f8cb8188d54772db39547d13589d309b6e988144
CreepyMemes <mjouins@gmail.com>
2025-01-04 15:48:13 +0100

    fixed button markup

--- src/components/pagination/PaginationButton.js
+++ src/components/pagination/PaginationButton.js
@@ -13,10 +13,16 @@ import Icon from '../common/Icon';
 
 class PaginationButton extends Component {
   static markup({ page, side }) {
+    const iconMarkup = Icon.markup({
+      iconClass: 'search__icon',
+      iconName: `icon-arrow-${side}`,
+    });
+
+    const spanMarkup = /* html */ `<span>Page ${page}</span>`;
+
     return /* html */ `
       <button class="btn--inline pagination__btn--${side == 'left' ? 'prev' : 'next'}">
-        ${Icon.markup({ iconClass: 'search__icon', iconName: `icon-arrow-${side}` })}
-        <span>${page}</span>
+      ${side === 'left' ? `${iconMarkup}${spanMarkup}` : `${spanMarkup}${iconMarkup}`}
       </button>
     `;
   }
