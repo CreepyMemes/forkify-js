e5746a4a48a0386551fbf26eefb642a9c7ff8597
CreepyMemes <mjouins@gmail.com>
2025-01-04 09:24:10 +0100

    cleaned up App by reducing it's items to multiple components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logoPath from './img/logo.png';
-
 import Recipe from './components/recipe/Recipe';
 import SearchResults from './components/search-results/SearchResults';
 import Search from './components/search/Search';
-import Icon from './components/common/Icon';
 import Component from './components/Component';
 import Message from './components/common/Message';
+import Header from './components/header/Header';
+import AddRecipeWindow from './components/add-recipe-window/AddRecipeWindow';
 
 class App extends Component {
   constructor(container) {
@@ -26,52 +25,7 @@ class App extends Component {
     return /* html */ `
       <div class="container">
         <header class="header">
-          <img src="${logoPath}" alt="Logo" class="header__logo" />
-
-          <form class="search">
-            ${Search.markup()}
-          </form>
-
-          <nav class="nav">
-            <ul class="nav__list">
-              <li class="nav__item">
-                <button class="nav__btn nav__btn--add-recipe">
-                  ${Icon.markup({ iconClass: 'nav__icon', iconName: 'icon-edit' })}
-                  <span>Add recipe</span>
-                </button>
-              </li>
-              <li class="nav__item">
-                <button class="nav__btn nav__btn--bookmarks">
-                  ${Icon.markup({ iconClass: 'nav__icon', iconName: 'icon-bookmark' })}
-                  <span>Bookmarks</span>
-                </button>
-                <div class="bookmarks">
-                  <ul class="bookmarks__list">
-                    <div class="message">
-                      <div>
-                        ${Icon.markup({ iconName: 'icon-smile' })}
-                      </div>
-                      <p>No bookmarks yet. Find a nice recipe and bookmark it :)</p>
-                    </div>
-
-                    <!-- <li class="preview">
-                      <a class="preview__link" href="#23456">
-                        <figure class="preview__fig">
-                          <img src="src/img/test-1.jpg" alt="Test" />
-                        </figure>
-                        <div class="preview__data">
-                          <h4 class="preview__name">
-                            Pasta with Tomato Cream ...
-                          </h4>
-                          <p class="preview__publisher">The Pioneer Woman</p>
-                        </div>
-                      </a>
-                    </li> -->
-                  </ul>
-                </div>
-              </li>
-            </ul>
-          </nav>
+          ${Header.markup()}
         </header>
 
         <div class="search-results-container">
@@ -84,52 +38,9 @@ class App extends Component {
       </div>
 
       <div class="overlay hidden"></div>
+
       <div class="add-recipe-window hidden">
-        <button class="btn--close-modal">&times;</button>
-        <form class="upload">
-          <div class="upload__column">
-            <h3 class="upload__heading">Recipe data</h3>
-            <label>Title</label>
-            <input value="TEST" required name="title" type="text" />
-            <label>URL</label>
-            <input value="TEST" required name="sourceUrl" type="text" />
-            <label>Image URL</label>
-            <input value="TEST" required name="image" type="text" />
-            <label>Publisher</label>
-            <input value="TEST" required name="publisher" type="text" />
-            <label>Prep time</label>
-            <input value="23" required name="cookingTime" type="number" />
-            <label>Servings</label>
-            <input value="23" required name="servings" type="number" />
-          </div>
-
-          <div class="upload__column">
-            <h3 class="upload__heading">Ingredients</h3>
-            <label>Ingredient 1</label>
-            <input
-              value="0.5,kg,Rice"
-              type="text"
-              required
-              name="ingredient-1"
-              placeholder="Format: 'Quantity,Unit,Description'"
-            />
-            <label>Ingredient 2</label>
-            <input value="1,,Avocado" type="text" name="ingredient-2" placeholder="Format: 'Quantity,Unit,Description'" />
-            <label>Ingredient 3</label>
-            <input value=",,salt" type="text" name="ingredient-3" placeholder="Format: 'Quantity,Unit,Description'" />
-            <label>Ingredient 4</label>
-            <input type="text" name="ingredient-4" placeholder="Format: 'Quantity,Unit,Description'" />
-            <label>Ingredient 5</label>
-            <input type="text" name="ingredient-5" placeholder="Format: 'Quantity,Unit,Description'" />
-            <label>Ingredient 6</label>
-            <input type="text" name="ingredient-6" placeholder="Format: 'Quantity,Unit,Description'" />
-          </div>
-
-          <button class="btn upload__btn">
-            ${Icon.markup({ iconName: 'icon-upload-cloud' })}
-            <span>Upload</span>
-          </button>
-        </form>
+        ${AddRecipeWindow.markup()}
       </div>
     `;
   }
---
+++ src/components/header/Header.js
@@ -0,0 +1,67 @@
+import logoPath from '../../img/logo.png';
+
+import Component from '../Component';
+import Icon from '../common/Icon';
+import Search from '../search/Search';
+
+class Header extends Component {
+  constructor(container) {
+    super(container);
+
+    // this.somehting = new Something(idk)
+  }
+
+  static markup() {
+    return /* html */ `
+
+      <img src="${logoPath}" alt="Logo" class="header__logo" />
+
+      <form class="search">
+        ${Search.markup()}
+      </form>
+
+      <nav class="nav">
+        <ul class="nav__list">
+          <li class="nav__item">
+            <button class="nav__btn nav__btn--add-recipe">
+              ${Icon.markup({ iconClass: 'nav__icon', iconName: 'icon-edit' })}
+              <span>Add recipe</span>
+            </button>
+          </li>
+          <li class="nav__item">
+            <button class="nav__btn nav__btn--bookmarks">
+              ${Icon.markup({ iconClass: 'nav__icon', iconName: 'icon-bookmark' })}
+              <span>Bookmarks</span>
+            </button>
+            <div class="bookmarks">
+              <ul class="bookmarks__list">
+                <div class="message">
+                  <div>
+                    ${Icon.markup({ iconName: 'icon-smile' })}
+                  </div>
+                  <p>No bookmarks yet. Find a nice recipe and bookmark it :)</p>
+                </div>
+
+                <!-- <li class="preview">
+                  <a class="preview__link" href="#23456">
+                    <figure class="preview__fig">
+                      <img src="src/img/test-1.jpg" alt="Test" />
+                    </figure>
+                    <div class="preview__data">
+                      <h4 class="preview__name">
+                        Pasta with Tomato Cream ...
+                      </h4>
+                      <p class="preview__publisher">The Pioneer Woman</p>
+                    </div>
+                  </a>
+                </li> -->
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </nav>
+    `;
+  }
+}
+
+export default Header;
