aa988f86c72b472976fc6ffabccd1f82f4fe56f9
CreepyMemes <mjouins@gmail.com>
2025-01-05 08:07:31 +0100

    forgot to remove comments

--- src/components/pagination/PaginationButton.js
+++ src/components/pagination/PaginationButton.js
@@ -1,16 +1,6 @@
 import Component from '../Component';
 import Icon from '../common/Icon';
 
-// <button class="btn--inline pagination__btn--prev">
-//         ${Icon.markup({ iconClass: 'search__icon', iconName: 'icon-arrow-left' })}
-//         <span>${page - 1}</span>
-//       </button>
-
-//       <button class="btn--inline pagination__btn--next">
-//         <span>${page + 1}</span>
-//         ${Icon.markup({ iconClass: 'search__icon', iconName: 'icon-arrow-right' })}
-//       </button>
-
 class PaginationButton extends Component {
   static markup({ page, side }) {
     const iconMarkup = Icon.markup({
